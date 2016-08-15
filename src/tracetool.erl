8aa8b93b040a83468fe628cbbd7d2663e909922b
zegang <zegang.luo@gmail.com>
2016-08-15 13:22:59 +0800

    Add recon project and custom tracetool codes

---
+++ src/tracetool.erl
@@ -0,0 +1,120 @@
+%% @author ezegluo
+%% @doc @todo Add description to tracetool.
+
+
+-module(tracetool).
+
+-define(dbname, tracetool_db).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([stop/0, start/1, trace/3, clear/0]).
+
+start(ConfFilePath) ->
+    {ok, ConfigList} = file:consult(ConfFilePath),
+	ets:new(?dbname, [public, named_table, compressed]),
+	lists:foreach(
+	  fun({trace, Node, Specs, Max, Options})->
+			  rpc:call(Node, tracetool, trace, [Specs, Max, Options]),
+			  record_node(Node)
+	  end,
+	  ConfigList).
+
+trace(Specs, Max, Options) ->
+	case ets:info(?dbname) of
+		undefined ->
+			ets:new(?dbname, [public, named_table, compressed])
+	end,
+	NewOpts = modify_options_for_recon(Options),
+	recon_trace:calls(Specs, Max, NewOpts).
+
+stop() -> 
+    Nodes = ets:lookup(?dbname, enodes),
+	case Nodes of
+		[] ->
+            ok;
+        Nodes ->
+			lists:foreach(
+	            fun(Node)->
+			        rpc:call(Node, tracetool, clear, []),
+			        remove_node(Node)
+	                end,
+	            Nodes)
+	end.
+
+clear() ->
+	close_openresource(),
+	recon_trace:clear().
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+close_openresource() ->
+    case ets:lookup(?dbname, handlers) of
+		[] ->
+			ok;
+		Handlers - >
+			lists:foreach(
+		        fun(Handler) -> 
+						case Handler of
+							{logfile, Dev} ->
+								file:close(Dev);
+							true ->
+								ok
+						end
+				end,
+			Handlers)
+    end.
+
+record_node(Node) ->
+    Nodes = ets:lookup(?dbname, enodes),
+	case Nodes of
+		[] ->
+            ets:insert(?dbname, {enodes, [Node]});
+        Nodes ->
+			NewNodes = Nodes ++ Node,
+			ets:insert(?dbname, {enodes, NewNodes})
+	end.
+
+remove_node(Node) ->
+    Nodes = ets:lookup(?dbname, enodes),
+	case Nodes of
+		[] ->
+            ok;
+        Nodes ->
+			NewNodes = Nodes -- Node,
+			ets:insert(?dbname, {enodes, NewNodes})
+	end.
+
+%%% Store handler of opened resources in DB
+record_handler(Handler) ->
+	Handlers = ets:lookup(?dbname, handlers),
+	case Handlers of
+		[] ->
+            ets:insert(?dbname, {handlers, [Handler]});
+        Handlers ->
+			NewHandlers = Handlers ++ Handler,
+			ets:insert(?dbname, {handlers, NewHandlers})
+	end.
+
+remove_handler(Handler) ->
+    Handlers = ets:lookup(?dbname, handlers),
+	case Handlers of
+		[] ->
+            ok;
+        Handlers ->
+			NewHandlers = Handlers -- Handler,
+			ets:insert(?dbname, {handlers, NewHandlers})
+	end.
+
+modify_options_for_recon(Options) -> 
+	lists:foldl(
+	  fun(Opt, NewOptions) ->
+			  case Opt of
+				  {logfilepath, LogfilePath} ->
+					  {ok, Dev} = file:open(LogfilePath ++ "tracetool_" ++ node() ++ ".log",[write]),
+					  NewOptions ++ {io_server, Dev},
+					  record_handler({logfile, Dev});
+				  Opt ->
+					  NewOptions ++ Opt
+			  end, [], Options).
