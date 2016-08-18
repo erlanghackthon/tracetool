8cd7874824371a6b8cd63608758856989896f761
Xinyu Liu <xinyu.liu@ericsson.com>
2016-08-18 13:54:10 +0200

    new trace_mgr
    Change-Id: Iac524ea319bafc30d6cc31c5df67688c3397be0c

---
+++ src/trace_mgr.erl
@@ -0,0 +1,198 @@
+-module(trace_mgr).
+
+-behaviour(gen_server).
+
+%% API functions
+-export([start_link/0,
+         start/0,
+         create_db/1,
+         create_db/2,
+         destroy_db/1,
+         open_report/1,
+         close_report/0,
+         get_value/2,
+         update_value/3
+         ]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {fd}).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start() ->
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, #state{}}.
+
+create_db(DBName) ->
+    gen_server:call(?MODULE, {create_db, DBName}).
+create_db(DBName, Options) when is_list(Options)->
+    gen_server:call(?MODULE, {create_db, DBName, Options}).
+destroy_db(DBName) ->
+    gen_server:call(?MODULE, {destroy_db, DBName}).
+
+open_report(ReportName) ->
+    gen_server:call(?MODULE, {open_report, ReportName}).
+close_report() ->
+    gen_server:call(?MODULE, close_report).
+
+get_value(DBName, Key) ->
+    gen_server:call(?MODULE, {get_entry, DBName, Key}).
+update_value(DBName, Key, Value) ->
+    gen_server:call(?MODULE, {update_entry, DBName, Key, Value}).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({create_db, DBName}, _From, State) ->
+    TableName = ets:new(DBName, [public, named_table, compressed]),
+    {reply, TableName, State};
+handle_call({create_db, DBName, Options}, _From, State) ->
+    TableName = ets:new(DBName, Options),
+    {reply, TableName, State};
+handle_call({destroy_db, DBName}, _From, State) ->
+    RtnVar = ets:delete(DBName),
+    {reply, RtnVar, State};
+
+handle_call({open_report, ReportName}, _From, State) ->
+    {ok, Fd} = file:open(ReportName, [write]),
+    file:write(Fd, "-----------Tracing Started---------------~n"),
+    {reply, ok, State#state{fd = Fd}};
+handle_call(close_report, _From, State) ->
+    file:close(State#state.fd),
+    {reply, ok, State};
+
+handle_call({get_entry, DBName, Key}, _From, State) ->
+    Reply =
+    case ets:info(DBName) of
+        undefined ->
+            {error, enotable};
+        _Other ->
+            case ets:lookup(DBName, Key) of
+                [] ->
+                    {error, enoentry};
+                [{Key, Value}] ->
+                    {ok, Value}
+            end
+    end,
+    {reply, Reply, State};
+
+handle_call({update_entry, DBName, Key, Value}, _From, State) ->
+    Reply =
+    case ets:info(DBName) of
+        undefined ->
+            {error, enotable};
+        _Other ->
+            case ets:update_element(DBName, Key, {2, Value}) of
+                true ->
+                    ok;
+                false ->
+                    {error, enoentry}
+            end
+    end,
+    {reply, Reply, State};
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
